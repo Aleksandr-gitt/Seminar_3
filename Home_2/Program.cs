bf06904e8fd24fc802196b2cd91a7c5764c3a562
Alex <sanyasan123@mail.ru>
2022-08-14 00:21:33 +0300

    add Home_2

--- Home_2/Program.cs
+++ Home_2/Program.cs
@@ -3,5 +3,21 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-Console.WriteLine("Введите число ");
-int arg = Console.ReadLine();
+Console.WriteLine("Введите координаты двух точек A и B в формате X/Y/Z :");
+Console.WriteLine("Введите координату Х точки A :");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки A :");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z точки A :");
+int Az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Х точки B :");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки B :");
+int By = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z точки B :");
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((Bx-Ax),2) + Math.Pow((By-Ay),2) + Math.Pow((Bz-Az),2));
+
+Console.WriteLine("Расстояние между указанными точками - " + result);
